3d90bd4e543c9af69bcf06ce3caa2575bc17f74c
insomniart <pwcahyo@gmail.com>
2016-01-20 16:17:51 +0700

    first push

---
+++ app/src/main/java/id/pwcahyo/gcm/MainActivity.java
@@ -0,0 +1,255 @@
+package id.pwcahyo.gcm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Resgistration Id from GCM
+    private static final String PREF_GCM_REG_ID = "PREF_GCM_REG_ID";
+    private SharedPreferences prefs;
+    // Your project number and web server url. Please change below.
+    private static final String GCM_SENDER_ID = "844112970816";
+    private static final String WEB_SERVER_URL = "http://phpserver-pwcahyo.rhcloud.com/register_user.php";
+
+    GoogleCloudMessaging gcm;
+    Button registerBtn;
+    TextView regIdView;
+
+    private static final int ACTION_PLAY_SERVICES_DIALOG = 100;
+    protected static final int MSG_REGISTER_WITH_GCM = 101;
+    protected static final int MSG_REGISTER_WEB_SERVER = 102;
+    protected static final int MSG_REGISTER_WEB_SERVER_SUCCESS = 103;
+    protected static final int MSG_REGISTER_WEB_SERVER_FAILURE = 104;
+    private String gcmRegId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerBtn = (Button) findViewById(R.id.register_gcmserver);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Check device for Play Services APK.
+                if (isGoogelPlayInstalled()) {
+                    gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+
+                    // Read saved registration id from shared preferences.
+                    gcmRegId = getSharedPreferences().getString(PREF_GCM_REG_ID, "");
+
+                    if (TextUtils.isEmpty(gcmRegId)) {
+                        handler.sendEmptyMessage(MSG_REGISTER_WITH_GCM);
+                    } else {
+                        regIdView.setText(gcmRegId);
+                        Toast.makeText(getApplicationContext(), "Already registered with GCM, web : "+WEB_SERVER_URL, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+        regIdView = (TextView) findViewById(R.id.regId);
+    }
+
+    private boolean isGoogelPlayInstalled() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        ACTION_PLAY_SERVICES_DIALOG).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Google Play Service is not installed",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        if (prefs == null) {
+            prefs = getApplicationContext().getSharedPreferences(
+                    "AndroidSRCDemo", Context.MODE_PRIVATE);
+        }
+        return prefs;
+    }
+
+    public void saveInSharedPref(String result) {
+        // TODO Auto-generated method stub
+        SharedPreferences.Editor editor = getSharedPreferences().edit();
+        editor.putString(PREF_GCM_REG_ID, result);
+        editor.commit();
+    }
+
+    Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case MSG_REGISTER_WITH_GCM:
+                    new GCMRegistrationTask().execute();
+                    break;
+                case MSG_REGISTER_WEB_SERVER:
+                    new WebServerRegistrationTask().execute();
+                    break;
+                case MSG_REGISTER_WEB_SERVER_SUCCESS:
+                    Toast.makeText(getApplicationContext(),
+                            "registered with web server", Toast.LENGTH_LONG).show();
+                    break;
+                case MSG_REGISTER_WEB_SERVER_FAILURE:
+                    Toast.makeText(getApplicationContext(),
+                            "registration with web server failed",
+                            Toast.LENGTH_LONG).show();
+                    break;
+            }
+        };
+    };
+
+    private class GCMRegistrationTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // TODO Auto-generated method stub
+            if (gcm == null && isGoogelPlayInstalled()) {
+                gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+            }
+            try {
+                gcmRegId = gcm.register(GCM_SENDER_ID);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return gcmRegId;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Toast.makeText(getApplicationContext(), "registered with GCM",
+                        Toast.LENGTH_LONG).show();
+                regIdView.setText(result);
+                saveInSharedPref(result);
+                handler.sendEmptyMessage(MSG_REGISTER_WEB_SERVER);
+            }
+        }
+
+    }
+
+    private class WebServerRegistrationTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            URL url = null;
+            try {
+                url = new URL(WEB_SERVER_URL);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                handler.sendEmptyMessage(MSG_REGISTER_WEB_SERVER_FAILURE);
+            }
+            Map<String, String> dataMap = new HashMap<String, String>();
+            dataMap.put("regId", gcmRegId);
+
+            StringBuilder postBody = new StringBuilder();
+            Iterator iterator = dataMap.entrySet().iterator();
+
+            while (iterator.hasNext()) {
+                Map.Entry param = (Map.Entry) iterator.next();
+                postBody.append(param.getKey()).append('=')
+                        .append(param.getValue());
+                if (iterator.hasNext()) {
+                    postBody.append('&');
+                }
+            }
+            String body = postBody.toString();
+            byte[] bytes = body.getBytes();
+
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setFixedLengthStreamingMode(bytes.length);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded;charset=UTF-8");
+
+                OutputStream out = conn.getOutputStream();
+                out.write(bytes);
+                out.close();
+
+                int status = conn.getResponseCode();
+                if (status == 200) {
+                    // Request success
+                    handler.sendEmptyMessage(MSG_REGISTER_WEB_SERVER_SUCCESS);
+                } else {
+                    throw new IOException("Request failed with error code "
+                            + status);
+                }
+            } catch (ProtocolException pe) {
+                pe.printStackTrace();
+                handler.sendEmptyMessage(MSG_REGISTER_WEB_SERVER_FAILURE);
+            } catch (IOException io) {
+                io.printStackTrace();
+                handler.sendEmptyMessage(MSG_REGISTER_WEB_SERVER_FAILURE);
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
